97778f5beefe79400002f33e4e451f69d5e05dd5
Minthos <minthos@gmail.com>
2020-04-14 12:26:44 +0700

    .

--- world.swift
+++ world.swift
@@ -13,37 +13,168 @@ let configString = """
 {
     "randomSeed": 1,
     "numGalaxies": 1,
-    "numSystems": 1,
-    "numPlanets": 8
+    "maxSystems": 1,
+    "maxPlanets": 20,
+    "maxMoons": 200,
+    "maxAsteroids": 100000000
 }
 """
 class Config: Decodable{
-    let randomSeed: UInt32
+    let randomSeed: Int
     let numGalaxies: Int
-    let numSystems: Int
-    let numPlanets: Int
+    let maxSystems: Int
+    let maxPlanets: Int
+    let maxMoons: Int
+    let maxAsteroids: Int
 }
 print("reading config")
-let configObject = try!JSONDecoder().decode(Config.self, from:configString.data(using: .utf8)!)
+let config = try!JSONDecoder().decode(Config.self, from:configString.data(using: .utf8)!)
+
+func qtyGen(max_: Int, min_: Int) -> Int{
+    let a = (random() % max_) / 3
+    let b = (random() % max_) / 3
+    let c = (random() % max_) / 3
+    return min(min_, a * b + c)
+}
 
 ////////////////////////////// CLASSES ////////////////////////////////////////
 
-class System{
-    private let randomSeed: UInt32!
 
-    init(seed: UInt32){
+
+class Ship{
+//    private let randomSeed: Int!
+    let id: Int!
+
+    init(seed: Int, id: Int){
+//        self.randomSeed = seed
+        self.id = id
+    }
+
+    func postInit(){
+//        srandom(UInt32(self.randomSeed))
+
+    }
+}
+
+class Station{
+//    private let randomSeed: Int!
+    let id: Int!
+
+    init(seed: Int, id: Int){
+        self.id = id
+//        self.randomSeed = seed
+    }
+
+    func postInit(){
+//        srandom(UInt32(self.randomSeed))
+
+    }
+}
+
+class Asteroid{
+    private let randomSeed: Int!
+    let id: Int!
+
+    init(seed: Int, id: Int){
+        self.randomSeed = seed
+        self.id = id
+    }
+
+    func postInit(){
+        srandom(UInt32(self.randomSeed))
+    }
+}
+
+class Moon{
+    private let randomSeed: Int!
+    let id: Int!
+
+    init(seed: Int, id: Int){
+        self.randomSeed = seed
+        self.id = id
+    }
+
+    func postInit(){
+        srandom(UInt32(self.randomSeed))
+
+    }
+}
+
+class Planet{
+    private let randomSeed: Int!
+    let id: Int!
+
+    init(seed: Int, id: Int){
+        self.randomSeed = seed
+        self.id = id
+    }
+
+    func postInit(){
+        srandom(UInt32(self.randomSeed))
+
+    }
+}
+
+class System{
+    private let randomSeed: Int!
+    let id: Int!
+
+    init(seed: Int, id: Int){
+        self.randomSeed = seed
+        self.id = id
+    }
+
+    func postInit(){
+        srandom(UInt32(self.randomSeed))
+
+    }
+}
+
+class Galaxy{
+    private let randomSeed: Int!
+    let id: Int!
+    var systems = [System]()
+
+    init(seed: Int, id: Int){
+        self.randomSeed = seed
+        self.id = id
+    }
+
+    func postInit(){
+        srandom(UInt32(self.randomSeed))
+        let numSystems = qtyGen(max_: config.maxSystems, min_:1)
+        for i in 0...numSystems{
+            systems.append(System(seed: random(), id:i))
+        }
+        for i in 0...numSystems{
+            systems[i].postInit()
+        }
+    }
+}
+
+class Universe{
+    private let randomSeed: Int!
+    var galaxies = [Galaxy]()
+
+    init(seed: Int){
         self.randomSeed = seed
     }
 
-    func postinit(){
-        srandom(self.randomSeed)
-
+    func postInit(){
+        srandom(UInt32(self.randomSeed))
+        let numGalaxies = qtyGen(max_: config.numGalaxies, min_:1)
+        for i in 0...numGalaxies{
+            self.galaxies.append(Galaxy(seed: random(), id:i))
+        }
+        for i in 0...numGalaxies{
+            self.galaxies[i].postInit()
+        }
     }
 }
 
 ////////////////////////////// INITIALIZATION ////////////////////////////////
 
-print("initializing world with seed: \(configObject.randomSeed) at time: \(Date()) ");
+print("initializing world with seed: \(config.randomSeed) at time: \(Date()) ");
 
 for _ in 0...3{
     print(random())
