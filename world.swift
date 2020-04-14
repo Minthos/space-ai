414a04cc4e5c3ea44038c71764e5fe8eb50efb12
Minthos <minthos@gmail.com>
2020-04-14 17:48:09 +0700

    coordinate generation added

--- world.swift
+++ world.swift
@@ -1,19 +1,24 @@
 ////////////////////////////// COPYRIGHT //////////////////////////////////////
 // Copyright 2020 Espen Overaae. All rights reserved.
-// Noncommercial use/modification/distribution permitted until further notice.
+// 
 
 
 ////////////////////////////// IMPORTS ////////////////////////////////////////
 import Foundation
 import Glibc
 
+let PARSEC = 3.086e16
+let LY = 9.461e15
+let AU = 1.496e11
+let KM = 1e3
+
 ////////////////////////////// CONFIG /////////////////////////////////////////
 
 let configString = """
 {
-    "randomSeed": 1,
+    "randomSeed": 3,
     "numGalaxies": 2,
-    "maxSystems": 10,
+    "maxSystems": 5,
     "maxPlanets": 40,
     "maxMoons": 100,
     "maxAsteroids": 1000000
@@ -50,35 +55,48 @@ func genericQtyGen(max_: Int, min_: Int) -> Int{
     return max(min_, (a * b) / max_)
 }
 
+func coordGen(rho_scaling: Double) -> (Double, Double, Double){
+    var rho = Double(random()) / Double(RAND_MAX)
+    let rho2 = Double(random()) / Double(RAND_MAX)
+    let theta = Double(random()) / Double(RAND_MAX)
+    var phi = Double(random()) / Double(RAND_MAX)
+    let phi2 = Double(random()) / Double(RAND_MAX)
+    phi = (phi - 0.5) * 2 * phi2
+    rho = rho * rho2
+    return (rho * rho_scaling, theta * 360, phi * 360)
+}
+
 ////////////////////////////// CLASSES ////////////////////////////////////////
 
 class Ship{
-//    private let randomSeed: Int!
     let id: Int!
 
+    var rho: Double = 0
+    var theta: Double = 0
+    var phi: Double = 0
+
     init(seed: Int, id: Int){
-//        self.randomSeed = seed
         self.id = id
     }
 
-    func postPostInit(){
-//        srandom(UInt32(self.randomSeed))
-
+    func postInit(){
     }
 }
 
 class Station{
-//    private let randomSeed: Int!
     let id: Int!
+
+    var rho: Double = 0
+    var theta: Double = 0
+    var phi: Double = 0
 
     init(seed: Int, id: Int){
         self.id = id
-//        self.randomSeed = seed
     }
 
-    func postPostInit(){
-//        srandom(UInt32(self.randomSeed))
+    func postInit(){
         print("Space Station!")
+        (self.rho, self.theta, self.phi) = coordGen(rho_scaling: 1e6 * KM)
     }
 }
 
@@ -86,19 +104,45 @@ class Asteroid{
     private let randomSeed: Int!
     let id: Int!
 
+    var rho: Double = 0
+    var theta: Double = 0
+    var phi: Double = 0
+
+    var minerals: Double = 0
+    var gas: Double = 0
+    var precious: Double = 0
+
     init(seed: Int, id: Int){
         self.randomSeed = seed
         self.id = id
     }
 
-    func postPostInit(){
+    func postInit(){
         srandom(UInt32(self.randomSeed))
+        
+        (self.rho, self.theta, self.phi) = coordGen(rho_scaling: 100 * AU)
+
+        let type = random()
+        if type % 5 > 0{
+            minerals = Double(random()) / Double(RAND_MAX)
+        }
+        if type % 5 < 3{
+            gas = Double(random()) / Double(RAND_MAX)
+        }
+        if type % 16 == 0{
+            precious = Double(random()) / Double(RAND_MAX)
+        }
     }
 }
 
 class Moon{
     private let randomSeed: Int!
     let id: Int!
+    
+    var rho: Double = 0
+    var theta: Double = 0
+    var phi: Double = 0
+
     var stations = [Station]()
 
     init(seed: Int, id: Int){
@@ -106,14 +150,15 @@ class Moon{
         self.id = id
     }
 
-    func postPostInit(){
+    func postInit(){
         srandom(UInt32(self.randomSeed))
+        (self.rho, self.theta, self.phi) = coordGen(rho_scaling: 1e8 * KM)
         let numStations = random() % 100 == 0 ? 1 : 0
         for i in 0..<numStations{
             stations.append(Station(seed: random(), id:i))
         }
         for i in 0..<numStations{
-            stations[i].postPostInit()
+            stations[i].postInit()
         }
 
     }
@@ -122,6 +167,11 @@ class Moon{
 class Planet{
     private let randomSeed: Int!
     let id: Int!
+    
+    var rho: Double = 0
+    var theta: Double = 0
+    var phi: Double = 0
+    
     var moons = [Moon]()
     var stations = [Station]()
 
@@ -130,24 +180,22 @@ class Planet{
         self.id = id
     }
 
-    func postPostInit(){
+    func postInit(){
         srandom(UInt32(self.randomSeed))
+        (self.rho, self.theta, self.phi) = coordGen(rho_scaling: 20 * AU)
         let numMoons = moonQtyGen(max_: config.maxMoons, min_:0)
         let numStations = random() % 10 == 0 ? 1 : 0
-        if numMoons > 0{
-            print("spawning \(numMoons) moons")
-        }
         for i in 0..<numMoons{
             moons.append(Moon(seed: random(), id:i))
-        }
-        for i in 0..<numMoons{
-            moons[i].postPostInit()
         }
         for i in 0..<numStations{
             stations.append(Station(seed: random(), id:i))
         }
+        for i in 0..<numMoons{
+            moons[i].postInit()
+        }
         for i in 0..<numStations{
-            stations[i].postPostInit()
+            stations[i].postInit()
         }
 
     }
@@ -156,6 +204,11 @@ class Planet{
 class System{
     private let randomSeed: Int!
     let id: Int
+    
+    var rho: Double = 0
+    var theta: Double = 0
+    var phi: Double = 0
+    
     var planets = [Planet]()
     var asteroids = [Asteroid]()
 
@@ -164,33 +217,35 @@ class System{
         self.id = id
     }
 
-    func postPostInit(){
+    func postInit(){
         print("system \(self.id)")
         srandom(UInt32(self.randomSeed))
+        (self.rho, self.theta, self.phi) = coordGen(rho_scaling: 1.0e6 * PARSEC)
         let numPlanets = planetQtyGen(max_: config.maxPlanets, min_:1)
         let numAsteroids = genericQtyGen(max_: config.maxAsteroids, min_:1)
-        print("spawning \(numPlanets) planets")
         for i in 0..<numPlanets{
             planets.append(Planet(seed: random(), id:i))
         }
-        for i in 0..<numPlanets{
-            planets[i].postPostInit()
-        }
-        print("\(numPlanets) planets ready")
-        print("spawning \(numAsteroids) asteroids")
         for i in 0..<numAsteroids{
             asteroids.append(Asteroid(seed: random(), id:i))
         }
-        for i in 0..<numAsteroids{
-            asteroids[i].postPostInit()
+        for i in 0..<numPlanets{
+            planets[i].postInit()
         }
-        print("\(numAsteroids) asteroids ready")
+        for i in 0..<numAsteroids{
+            asteroids[i].postInit()
+        }
     }
 }
 
 class Galaxy{
     private let randomSeed: Int!
     let id: Int!
+    
+    var rho: Double = 0
+    var theta: Double = 0
+    var phi: Double = 0
+    
     var systems = [System]()
 
     init(seed: Int, id: Int){
@@ -198,16 +253,18 @@ class Galaxy{
         self.id = id
     }
 
-    func postPostInit(){
+    func postInit(){
         srandom(UInt32(self.randomSeed))
-        let numSystems = genericQtyGen(max_: config.maxSystems, min_:1)
+        (self.rho, self.theta, self.phi) = coordGen(rho_scaling: 1.4e9 * PARSEC)
+        //let numSystems = genericQtyGen(max_: config.maxSystems, min_:1)
+        let numSystems = max(1, random() % config.maxSystems)
         print("spawning \(numSystems) systems")
         for i in 0..<numSystems{
             systems.append(System(seed: random(), id:i))
         }
         print("configuring systems")
         for i in 0..<numSystems{
-            systems[i].postPostInit()
+            systems[i].postInit()
         }
         print("\(numSystems) systems ready")
     }
@@ -221,7 +278,7 @@ class Universe{
         self.randomSeed = seed
     }
 
-    func postPostInit(){
+    func postInit(){
         srandom(UInt32(self.randomSeed))
         let numGalaxies = config.numGalaxies
         print("spawning \(numGalaxies) galaxies")
@@ -230,7 +287,7 @@ class Universe{
         }
         print("configuring \(numGalaxies) galaxies")
         for i in 0..<numGalaxies{
-            self.galaxies[i].postPostInit()
+            self.galaxies[i].postInit()
         }
         print("\(numGalaxies) galaxies ready")
     }
@@ -248,9 +305,12 @@ if(CommandLine.arguments.count > 1){
 }
 print("initializing world with seed: \(seed) at time: \(Date()) ");
 let world = Universe(seed: seed)
-world.postPostInit()
+world.postInit()
 
 ////////////////////////////// RUNLOOP ///////////////////////////////////////
 
-
+//while true {
+//    print("zzz...")
+//    sleep(1);
+//}
 
